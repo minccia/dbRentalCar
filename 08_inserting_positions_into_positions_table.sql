27f1ef8bd35bdea9e4c32ac711004ad0c3df5926
minccia <lusyvasilva@gmail.com>
2022-12-18 16:22:59 -0300

    Inserting positions into POSITIONS table

---
+++ 08_inserting_positions_into_positions_table.sql
@@ -0,0 +1,7 @@
+-- SQLite
+
+-- Inserting positions into POSITIONS table 
+
+INSERT INTO POSITIONS (DESCRIPTION) 
+    VALUES ('Gerente de vendas'), ('Gerente de compras'), ('Vendedor'),
+           ('Mecânico'), ('Assistente Administrativo');
