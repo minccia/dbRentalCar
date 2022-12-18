1a727e5d6724fee43ad6727077d7400ca21522b7
minccia <lusyvasilva@gmail.com>
2022-12-18 15:44:21 -0300

    Insert objects into CUSTOMERS and CAR_MODELS tables

---
+++ 04_inserting_car_models_into_car_models_table.sql
@@ -0,0 +1,7 @@
+-- SQLite
+
+-- Inserting car_models into CAR_MODELS table 
+
+INSERT INTO CAR_MODELS (MODEL_NAME)
+    VALUES ('Conversível'), ('Sedã'), ('Hatch'), ('Coupé'), ('Perua'),
+           ('SUV'), ('Picape'), ('Minivan'), ('Utilitário'), ('Buggy');
