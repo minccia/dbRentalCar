3fda76be173d2e97e14c3f12cdfbd4cda307e9ee
minccia <lusyvasilva@gmail.com>
2022-12-18 16:18:16 -0300

    Inserting car_brands into CAR_BRAND table

---
+++ 06_inserting_car_brands_into_car_brand_table.sql
@@ -0,0 +1,7 @@
+-- SQLite
+
+-- Inserting car brands into CAR_BRAND table
+
+INSERT INTO CAR_BRAND (BRAND_NAME)
+    VALUES ('Chevrolet'), ('Toyota'), ('Hyundai'), ('Volkswagen'),
+           ('Jeep'), ('Renault'), ('Honda'), ('Fiat');
