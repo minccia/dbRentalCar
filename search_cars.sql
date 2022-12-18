3bb3aef4c8bf89346cceaf08d599e684751e2369
minccia <lusyvasilva@gmail.com>
2022-12-18 17:15:26 -0300

    Search cars

---
+++ search_cars.sql
@@ -0,0 +1,11 @@
+-- SQLite
+
+-- Search cars that have "Liberado" status
+
+SELECT * FROM CARS
+    WHERE STATUS = 'Liberado'
+
+-- Search cars from year 2016
+
+SELECT * FROM CARS 
+    WHERE YEAR = 2016
